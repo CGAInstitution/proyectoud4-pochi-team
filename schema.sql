c3094b5e7ffdc7a9d8d74d8d3d3c3da02cb926dc
joeelfgrd <joelfigueiridomolares@gmail.com>
2025-02-07 13:42:49 +0100

    feat(Paciente...):crear clases de paciente y hacer su crud

--- schema.sql
+++ schema.sql
@@ -1,4 +1,7 @@
+DROP DATABASE IF EXISTS HEALTH_DATABASE;
+CREATE DATABASE IF NOT EXISTS HEALTH_DATABASE;
 
+use HEALTH_DATABASE;
 
 CREATE TABLE enfermedad (
                             id bigint NOT NULL PRIMARY KEY,
@@ -13,15 +16,28 @@ CREATE TABLE medicamento (
                              id bigint NOT NULL PRIMARY KEY,
                              nombre varchar(500) NOT NULL,
                              descripcion varchar(500),
-                             precio int,
-                             recetable boolean NOT NULL
+                             precio int default 0,
+                             recetable boolean NOT NULL DEFAULT true
 );
 
 
 CREATE TABLE enfermedad_medicamento (
                                         id bigint NOT NULL PRIMARY KEY,
                                         id_enfermedad bigint NOT NULL,
-                                        id_medicamento bigint NOT NULL
+                                        id_medicamento bigint NOT NULL,
+                                        FOREIGN KEY (id_enfermedad) references enfermedad(id)
+											on delete cascade 
+											on update cascade,
+                                        FOREIGN KEY (id_medicamento) references medicamento(id) 
+											on delete cascade 
+											on update cascade
+);
+
+
+CREATE TABLE tarjeta (
+                         id bigint NOT NULL PRIMARY KEY,
+                         tarjeta_banco varchar(500) NOT NULL,
+                         recaudado bigint NOT NULL DEFAULT 0
 );
 
 
@@ -31,14 +47,14 @@ CREATE TABLE paciente (
                           edad int,
                           nombre varchar(500) NOT NULL,
                           tarjeta bigint NOT NULL,
-                          enfermedad bigint NOT NULL
-);
-
-
-CREATE TABLE tarjeta (
-                         id bigint NOT NULL PRIMARY KEY,
-                         tarjeta_banco varchar(500) NOT NULL,
-                         recaudado bigint NOT NULL
+                          enfermedad bigint NOT NULL,
+						  FOREIGN KEY (enfermedad) references enfermedad(id) 
+							on delete cascade 
+							on update cascade,
+						  FOREIGN KEY (tarjeta) references tarjeta(id) 
+							on delete cascade 
+							on update cascade
+                          
 );
 
 
@@ -48,11 +64,5 @@ CREATE TABLE usuario_data (
                               nombre varchar(500) NOT NULL,
                               password varchar(500) NOT NULL,
                               fecha_nacimiento date,
-                              donado bigint NOT NULL
+                              donado bigint NOT NULL DEFAULT 0
 );
-
-
-ALTER TABLE enfermedad ADD CONSTRAINT enfermedad_id_fk FOREIGN KEY (id) REFERENCES enfermedad_medicamento (id_enfermedad);
-ALTER TABLE enfermedad_medicamento ADD CONSTRAINT enfermedad_medicamento_id_medicamento_fk FOREIGN KEY (id_medicamento) REFERENCES medicamento (id);
-ALTER TABLE paciente ADD CONSTRAINT paciente_tarjeta_fk FOREIGN KEY (tarjeta) REFERENCES tarjeta (id);
-ALTER TABLE paciente ADD CONSTRAINT paciente_enfermedad_fk FOREIGN KEY (enfermedad) REFERENCES enfermedad (id);
