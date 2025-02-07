f7050f5bb93d10c87d961be3391e4b722ee6918e
AmarinoLama <amarinolama@gmail.com>
2025-02-07 14:12:13 +0100

    feat(): realizar relación N:M entre enfermedad y medicamento

--- src/main/java/madstodolist/model/Medicamento.java
+++ src/main/java/madstodolist/model/Medicamento.java
@@ -1,6 +1,5 @@
 package madstodolist.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,15 +7,15 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
-import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "medicamentos")
-public class Medicamento implements Serializable {
+public class Medicamento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,6 +32,22 @@ public class Medicamento implements Serializable {
 
     @NotNull
     private boolean receta;
+
+    @ManyToMany
+    @JoinTable(
+            name = "paciente_enfermedad",
+            joinColumns = @JoinColumn(name = "paciente_id"),
+            inverseJoinColumns = @JoinColumn(name = "enfermedad_id")
+    )
+    private Set<Enfermedad> enfermedades = new HashSet<>();
+
+    public Medicamento(Long id, String nombre, @Null String descripcion, @Null int precio, boolean receta) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.receta = receta;
+    }
 
     public Medicamento(String nombre, @Null String descripcion, @Null int precio, boolean receta) {
         this.nombre = nombre;
