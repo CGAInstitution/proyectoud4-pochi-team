ffc173df6ae7981ad7b568c521d0a6ad04a13ef4
Victor <Victorsancheznogueira@gmail.com>
2025-02-09 21:26:19 +0100

    fix: header for all

--- src/main/java/madstodolist/controller/EnfermedadController.java
+++ src/main/java/madstodolist/controller/EnfermedadController.java
@@ -1,6 +1,9 @@
 package madstodolist.controller;
 
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.dto.UsuarioData;
 import madstodolist.service.EnfermedadService;
+import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +13,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EnfermedadController {
 
     @Autowired
+    ManagerUserSession managerUserSession;
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
     EnfermedadService enfermedadeService;
 
     @GetMapping("/enfermedades")
     public String enfermedades(Model model) {
+        Long usuarioLogeadoId = managerUserSession.usuarioLogeado();
+        boolean usuarioLogeado = usuarioLogeadoId != null;
+        model.addAttribute("usuarioLogeado", usuarioLogeado);
+
+        if (usuarioLogeado) {
+            UsuarioData usuario = usuarioService.findById(usuarioLogeadoId);
+            model.addAttribute("usuario", usuario);
+        }
         model.addAttribute("enfermedades", enfermedadeService.allEnfermedades());
         return "enfermedades";
     }
