caa2c9ba4b3139e3a31e0afcc3a0decfd180c673
Victor <Victorsancheznogueira@gmail.com>
2025-02-12 21:40:52 +0100

    feat: add accion de donar

--- src/main/java/madstodolist/controller/PacienteController.java
+++ src/main/java/madstodolist/controller/PacienteController.java
@@ -2,12 +2,18 @@ package madstodolist.controller;
 
 import madstodolist.authentication.ManagerUserSession;
 import madstodolist.dto.UsuarioData;
+import madstodolist.model.Paciente;
+import madstodolist.model.Tarjeta;
+import madstodolist.service.DonacionService;
 import madstodolist.service.PacienteService;
 import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PacienteController {
@@ -17,8 +23,12 @@ public class PacienteController {
 
     @Autowired
     UsuarioService usuarioService;
+
     @Autowired
     private PacienteService pacienteService;
+
+    @Autowired
+    private DonacionService donacionService;
 
     @GetMapping("/pacientes")
     public String pacientes(Model model) {
@@ -29,6 +39,23 @@ public class PacienteController {
         if (usuarioLogeado) {
             UsuarioData usuario = usuarioService.findById(usuarioLogeadoId);
             model.addAttribute("usuario", usuario);
+        }
+        model.addAttribute("pacientes", pacienteService.allPacientes());
+        return "Pacientes";
+    }
+
+    @PostMapping("/pacientes/donacion/{idPaciente}")
+    public String donar(@PathVariable Long idPaciente, @RequestParam int donativo, Model model) {
+        Long usuarioLogeadoId = managerUserSession.usuarioLogeado();
+        boolean usuarioLogeado = usuarioLogeadoId != null;
+        model.addAttribute("usuarioLogeado", usuarioLogeado);
+
+        if (usuarioLogeado) {
+            UsuarioData usuario = (usuarioService.findById(usuarioLogeadoId));
+            model.addAttribute("usuario", usuario);
+            Paciente paciente = pacienteService.findById(idPaciente);
+            Tarjeta tarjeta = paciente.getTarjeta();
+            donacionService.nuevaDonacion(tarjeta, usuarioService.getUsuario(usuario), donativo);
         }
         model.addAttribute("pacientes", pacienteService.allPacientes());
         return "Pacientes";
