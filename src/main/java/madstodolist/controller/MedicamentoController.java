83ae2820e2472d7849eda55816fc5947dd5597f6
AmarinoLama <amarinolama@gmail.com>
2025-02-12 18:25:05 +0100

    feat(): hacer la vista detallada de un medicamento

--- src/main/java/madstodolist/controller/MedicamentoController.java
+++ src/main/java/madstodolist/controller/MedicamentoController.java
@@ -2,12 +2,18 @@ package madstodolist.controller;
 
 import madstodolist.authentication.ManagerUserSession;
 import madstodolist.dto.UsuarioData;
+import madstodolist.model.Enfermedad;
+import madstodolist.model.Medicamento;
+import madstodolist.model.Paciente;
 import madstodolist.service.MedicamentoService;
 import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Set;
 
 @Controller
 public class MedicamentoController {
@@ -33,5 +39,24 @@ public class MedicamentoController {
         }
         model.addAttribute("medicamentos", medicamentoService.getAllMedicamentos());
         return "medicamentos";
+    }
+
+    @GetMapping("/medicamentos/{id}")
+    public String medicamentoDetallao(@PathVariable(value="id") Long idMedicamento, Model model) {
+
+        Long usuarioLogeadoId = managerUserSession.usuarioLogeado();
+        boolean usuarioLogeado = usuarioLogeadoId != null;
+        Medicamento medicamento = medicamentoService.getMedicamentoById(idMedicamento);
+        model.addAttribute("usuarioLogeado", usuarioLogeado);
+        model.addAttribute("medicamento", medicamento);
+        Set<Enfermedad> enfermedades = medicamento.getEnfermedades();
+        model.addAttribute("enfermedades", enfermedades);
+
+        if (usuarioLogeado) {
+            UsuarioData usuario = usuarioService.findById(usuarioLogeadoId);
+            model.addAttribute("usuario", usuario);
+        }
+
+        return "infoDetalladaMedicamento";
     }
 }
